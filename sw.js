734abe299ad41ea714dd0bba731ec4a9846fbd2f
josewp30 <jose.escalona.15@gmail.com>
2026-04-10 21:27:58 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-// version 2 - 24/03/2026
+// version 3 - 2024-04-10 (Forzar refresco tras corregir bug loading)
 self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => event.waitUntil(clients.claim()));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName); // Limpiar cachés viejos
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  // Estrategia: Network only para evitar cacheo de datos obsoletos en esta etapa de corrección
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
