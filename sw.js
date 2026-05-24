0ca7285a73430243af9e89921173c5cf89a47e19
josewp30 <jose.escalona.15@gmail.com>
2026-05-24 19:48:03 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Bypass waiting to immediately update the SW
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => caches.delete(cacheName))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    // Development mode: Network only, completely bypass cache
+    event.respondWith(fetch(event.request));
+});
